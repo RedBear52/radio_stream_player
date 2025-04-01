cc3220af9aec83a849dfc4ae742cf0d478728729
Ryan Spearman <94654287+RedBear52@users.noreply.github.com>
2025-04-01 13:53:58 -0500

    initial commit | bare bones stream player

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container">
+    <StreamPlayer />
+  </div>
+</template>
+
+<script>
+import StreamPlayer from './components/StreamPlayer.vue'
+export default {
+  name: 'App',
+  components: {
+    StreamPlayer,
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/StreamPlayer.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="stream-player">
+    <img src="../assets/kdhx-logo.png" alt="kdhx-logo" width="100" />
+
+    <h1>Listen Live</h1>
+    <audio ref="audio"></audio>
+    <button :class="{ active: isPlaying }" @click="playStream">Play</button>
+    <button :class="{ active: !isPlaying }" @click="stopStream">Stop</button>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+const streamUrl = 'http://kdhx-ice.streamguys1.com/live'
+
+export default {
+  name: 'StreamPlayer',
+  setup() {
+    const audio = ref(null)
+    const isPlaying = ref(false)
+
+    const playStream = () => {
+      if (audio.value) {
+        audio.value.src = streamUrl
+        audio.value.play()
+        isPlaying.value = true
+      }
+    }
+
+    const stopStream = () => {
+      if (audio.value) {
+        audio.value.pause()
+        audio.value.src = ''
+        isPlaying.value = false
+      }
+    }
+
+    return {
+      audio,
+      isPlaying,
+      playStream,
+      stopStream,
+    }
+  },
+}
+</script>
+
+<style lang="css" scoped>
+.stream-player {
+  text-align: center;
+  margin: 20px;
+}
+h1 {
+  font-size: 2em;
+  margin-bottom: 20px;
+}
+
+button {
+  margin: 10px;
+  padding: 10px 20px;
+  font-size: 1em;
+  cursor: pointer;
+  border: none;
+  border-radius: 5px;
+  background-color: #e02024;
+  color: white;
+
+  transition: background-color 0.3s ease;
+}
+
+button:hover {
+  background-color: black;
+  color: white;
+}
+
+button.active {
+  background-color: #ccc; /* Muted color for active button */
+  color: white;
+  cursor: not-allowed; /* Optional: indicate the button is inactive */
+}
+</style>
